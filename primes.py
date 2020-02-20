462f13cd79eef5d886445941f3aa235b0dd4acad
Mismaah Abdulla <mismaahabdulla@gmail.com>
2020-02-20 11:01:27 +0800

    Add python and text files

---
+++ primes.py
@@ -0,0 +1,29 @@
+primes = open("primes.txt", "r")
+primesArray = primes.read().split(',')
+primes.close()
+for i in range(len(primesArray)):
+    primesArray[i] = int(primesArray[i])
+i = primesArray[-1] + 1
+def lessThan(n):
+    lessThan = []
+    for i in range(n):
+        if(n-i != n and n-i != 1):
+            lessThan.append(n-i)
+    return lessThan
+while True:
+    isPrime = False
+    for j in range(len(lessThan(i))):
+        if(i%lessThan(i)[j] == 0):
+            isPrime = False
+            break
+        else:
+            isPrime = True
+    if(isPrime):
+        primesArray.append(i)
+        print(i)
+        print(primesArray)
+        primesString = ','.join(str(i) for i in primesArray)
+        primes = open("primes.txt", "w")
+        primes.write(primesString)
+        primes.close()
+    i+=1
